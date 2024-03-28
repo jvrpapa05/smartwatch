e81c55870178f194046844e387c6e1a089f90d98
John Vince Papa <jvrpapa05@gmail.com>
2024-03-28 17:25:41 +0800

    Remove calculation inside finger_present() function.

--- main/sketch_mar16a/sketch_mar16a.ino
+++ main/sketch_mar16a/sketch_mar16a.ino
@@ -226,23 +226,6 @@ boolean finger_present(){
 
   if (checkForBeat(irValue) == true)
   {
-    //We sensed a beat!
-    long delta = millis() - lastBeat;
-    lastBeat = millis();
-
-    beatsPerMinute = 60 / (delta / 1000.0);
-
-    if (beatsPerMinute < 255 && beatsPerMinute > 20)
-    {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
-
-      //Take average of readings
-      beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
-        beatAvg += rates[x];
-      beatAvg /= RATE_SIZE;
-    }
   }
 
   if (irValue < 50000){
